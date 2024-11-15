db0f96e1f8b749c8d336258b02afbba8844e9f38
Stijn Servaes <142609421+ComradeStijn@users.noreply.github.com>
2024-11-15 18:14:35 +0100

    feat: Browse page

---
+++ public/modal.js
@@ -0,0 +1,26 @@
+const dialog = document.querySelector("#add-item-modal");
+const toggleButton = document.querySelector("#add-item-btn");
+const html = document.querySelector("html");
+const animationDuration = 400;
+
+toggleButton.addEventListener("click", () => {
+  html.classList.add("modal-is-open", "modal-is-opening");
+  dialog.showModal();
+
+  setTimeout(() => {
+    html.classList.remove("modal-is-opening");
+    document.addEventListener("click", modalCloser);
+  }, animationDuration);
+});
+
+function modalCloser(e) {
+  const article = dialog.querySelector("article");
+  if (!article.contains(e.target)) {
+    html.classList.remove("modal-is-open");
+    dialog.close();
+    e.currentTarget.removeEventListener("click", modalCloser);
+  }
+}
+
+const closeButton = dialog.querySelector('#close-modal');
+closeButton.addEventListener('click', () => dialog.close());
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,11 +2,43 @@ import { Router } from "express";
 
 const router = Router()
 
+const data = [
+  {
+    folder: "Default",
+    title: "test.png",
+    size: "5kb",
+    uploadAt: "23-4-5",
+    link: "https://www.google.com"
+  },
+  {
+    folder: "Default",
+    title: "test2.jpg",
+    size: "5kb",
+    uploadAt: "23-4-5",
+    link: "https://www.google.com"
+  },
+  {
+    folder: "Main",
+    title: "main.png",
+    size: "5kb",
+    uploadAt: "23-4-5",
+    link: "https://www.google.com"
+  },
+  {
+    folder: "Secondary",
+    title: "Secondary.jpg",
+    size: "5kb",
+    uploadAt: "23-4-5",
+    link: "https://www.google.com"
+  }
+]
+const folders = ["Default", "Main", "Secondary", "Third"]
+
 router.get('/', (req, res) => {
   res.render('index', { title: "Stijn Drive" });
 })
 router.get('/browse', (req, res) => {
-  res.render('browse', { title: "Stijn Drive"})
+  res.render('browse', { title: "Stijn Drive", data: data, folders: folders })
 })
 
 export default router
